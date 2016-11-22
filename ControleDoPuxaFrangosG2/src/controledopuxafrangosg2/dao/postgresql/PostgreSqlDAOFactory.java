11aa6d2319da2142bb3939899134d57c816fac08
Alexandre Romanelli <alexromanelli@gmail.com>
2016-11-22 21:20:14 -0200

    Aula 22/11

---
+++ ControleDoPuxaFrangosG2/src/controledopuxafrangosg2/dao/postgresql/PostgreSqlDAOFactory.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controledopuxafrangosg2.dao.postgresql;
+
+import controledopuxafrangosg2.dao.DAOFactory;
+import controledopuxafrangosg2.dao.ListaComandosDAO;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author romanelli
+ */
+public class PostgreSqlDAOFactory extends DAOFactory {
+    
+    private static final String DB_URL = 
+            "jdbc:postgresql://localhost:5432/puxafrangos";
+    private static final String DB_USER = "postgres";
+    private static final String DB_PASSWORD = "romanelli";
+    
+    private Connection con;
+    
+    public PostgreSqlDAOFactory() {
+        try {
+            
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(
+                    null, 
+                    "Não foi possível carregar o driver para PostgreSQL.",
+                    "Erro", 
+                    JOptionPane.ERROR_MESSAGE);
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(
+                    null, 
+                    "Ocorreu um erro ao conectar com o banco de dados.",
+                    "Erro", 
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    @Override
+    public ListaComandosDAO getListaComandosDAO() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ComandoDAO getComandoDAO() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
