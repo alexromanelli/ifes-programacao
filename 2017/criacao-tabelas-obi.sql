cc787dcdc1fcd0887cc58876c221cbae29658722
Alexandre Romanelli <alexromanelli@gmail.com>
2017-11-27 13:53:15 -0200

    Conclusão de script de criação de BD

--- 2017/criacao-tabelas-obi.sql
+++ 2017/criacao-tabelas-obi.sql
@@ -46,3 +46,88 @@ create table escola (
   foreign key (tipoescola) references tipoescola(codigo)
 );
 
+create table delegado (
+  codigo serial primary key,
+  nome varchar(50) not null,
+  email varchar(100) not null,
+  ddd integer not null,
+  telefone varchar(10) not null,
+  escola integer not null,
+  
+  foreign key (escola) references escola(codigo)
+);
+
+create table aluno (
+  codigo serial primary key,
+  nome varchar(50) not null,
+  datanascimento date not null,
+  escola integer not null,
+  anocurso integer not null,
+  nivelcurso integer not null,
+  nivelmodalidade integer not null,
+  
+  foreign key (escola) references escola(codigo),
+  foreign key (nivelcurso) references nivelcurso(codigo),
+  foreign key (nivelmodalidade) references nivelmodalidade(codigo)
+);
+
+create table prova (
+  codigo serial primary key,
+  fase integer not null,
+  nivelmodalidade integer not null,
+  dataprova date not null,
+  horarioinicio time not null,
+  horariotermino time not null,
+  cadernoquestoes varchar(200) not null,
+  totalpontos integer,
+  
+  foreign key (fase) references fase(codigo),
+  foreign key (nivelmodalidade) references nivelmodalidade(codigo)
+);
+
+create table questaoprova (
+  codigo serial primary key,
+  prova integer not null,
+  nomequestao varchar(50) not null,
+  valor integer not null,
+  
+  foreign key (prova) references prova(codigo)
+);
+
+create table resposta (
+  codigo serial primary key,
+  aluno integer not null,
+  prova integer not null,
+  questaoprova integer not null,
+  solucao varchar(200) not null,
+  linguagem integer not null,
+  pontos integer,
+  
+  foreign key (aluno) references aluno(codigo),
+  foreign key (prova) references prova(codigo),
+  foreign key (questaoprova) references questaoprova(codigo),
+  foreign key (linguagem) references linguagem(codigo)
+);
+
+create table resultado (
+  codigo serial primary key,
+  aluno integer not null,
+  prova integer not null,
+  faseprova integer not null,
+  pontosobtidos integer not null,
+  totalpontos integer not null,
+  nota integer not null,
+  
+  foreign key (aluno) references aluno(codigo),
+  foreign key (prova) references prova(codigo),
+  foreign key (faseprova) references fase(codigo)
+);
+
+create table classificacao (
+  aluno integer primary key,
+  notafaselocal integer not null,
+  notafaseestadual integer not null,
+  notafinal integer not null,
+  
+  foreign key (aluno) references aluno(codigo)
+);
